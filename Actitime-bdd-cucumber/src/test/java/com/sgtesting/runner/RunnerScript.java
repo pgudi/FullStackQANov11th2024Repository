5925a6054a63628a5dfa4e406a82e065674d6b4c
pgudi <prabhakargudi@gmail.com>
2025-03-12 11:47:43 +0530

    BDD Cucumber With Extent Reports

--- Actitime-bdd-cucumber/src/test/java/com/sgtesting/runner/RunnerScript.java
+++ Actitime-bdd-cucumber/src/test/java/com/sgtesting/runner/RunnerScript.java
@@ -1,15 +1,16 @@
 package com.sgtesting.runner;
 
+import com.sgtesting.utility.ApplicationIndependent;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions(
-        features = "src/test/java/resources/FeatureFiles",
+        features = "src/test/resources/FeatureFiles",
         glue = "com.sgtesting.stepdefinitions",
-        plugin = {"pretty",
-                "html:Reports/actitime-cucumber-report.html",
-                "json:Reports/cucumber.json"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         tags = "@Execute"
 )
 public class RunnerScript extends AbstractTestNGCucumberTests {
+
 }
